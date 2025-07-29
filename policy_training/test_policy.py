be5086b179012c0a2d6c29724a709ee79b387781
Nicolo Dellepiane <nicolo.dellepiane@mail.polimi.it>
2025-07-30 00:02:37 +0200

    Mediocre, but acceptable results: spot is almost walking

--- policy_training/test_policy.py
+++ policy_training/test_policy.py
@@ -4,18 +4,16 @@ from stable_baselines3 import PPO
 from SpotmicroEnv import SpotmicroEnv
 from standing_reward_function import reward_function, init_custom_state
 
-run = "ppo_stand2M-0"
 env = SpotmicroEnv(
     use_gui=True, 
     reward_fn=reward_function,
     init_custom_state=init_custom_state, 
-    src_save_file=f"states/statestand2M-2.pkl"
     )
 obs, _ = env.reset()
 
 # Load your trained model
-model = PPO.load(f"policies/ppo_stand2M-2")  # or path to your .zip
-env._total_steps_counter = 1_000_000
+model = PPO.load(f"policies/ppo_walk10M-2")  # or path to your .zip
+env._total_steps_counter = 10_000_000
 print(f"num steps: {env.num_steps}")
 
 # Run rollout
