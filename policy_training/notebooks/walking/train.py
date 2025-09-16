542eff36dd0ac19a08e6e3f6a68d0674ecb4e79f
Nicolo Dellepiane <nicolo.dellepiane@mail.polimi.it>
2025-09-16 22:58:46 +0200

    modified training script to train trotting -> walking with rough terrain

--- policy_training/notebooks/walking/train.py
+++ policy_training/notebooks/walking/train.py
@@ -22,7 +22,7 @@ from stable_baselines3.common.env_checker import check_env
 from stable_baselines3.common.logger import configure
 
 # ========= CONFIG ==========
-TOTAL_STEPS = 16_000_000
+TOTAL_STEPS = 10_000_000
 run = "walk"
 base="trot"
 
@@ -34,7 +34,7 @@ def clipped_linear_schedule(initial_value, min_value=1e-5):
     return schedule
 
 checkpoint_callback = CheckpointCallback(
-    save_freq=TOTAL_STEPS // 16,
+    save_freq=TOTAL_STEPS // 10,
     save_path=f"{run}_checkpoints",
     name_prefix=f"ppo_{run}"
 )
