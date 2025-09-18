10f95353e18a1da9a6a1a92d324785150b766376
Nicolo Dellepiane <nicolo.dellepiane@mail.polimi.it>
2025-09-18 09:59:54 +0200

    removed effort penalty from trotting/rw_fn since afte normalization of ang_vel tracking penalty, it likely reduced trotting to shuffling. Training exp #3 to try achieving trotting again, but with better directional control

--- policy_training/notebooks/trotting/reward_function.py
+++ policy_training/notebooks/trotting/reward_function.py
@@ -23,11 +23,8 @@ def reward_function(env: SpotmicroEnv, action: np.ndarray) -> tuple[float, dict]
     ang_vel_error = np.linalg.norm((env.target_ang_velocity - env.agent.state.angular_velocity) / env.config.max_angular_velocity)** 2
     deviation_penalty = np.linalg.norm(env.agent.state.joint_positions - env.agent.homing_positions) ** 2
     height_penalty = (env.agent.state.base_position[2] - env.config.target_height) ** 2
-    action_rate = np.mean(action - env.agent.previous_action) ** 2
-    vertical_velocity_sq =  env.agent.state.linear_velocity[2] ** 2
     stabilization_penalty = roll ** 2 + pitch ** 2
     perp_velocity = env.agent.state.linear_velocity - ((np.dot(env.agent.state.linear_velocity, env.target_lin_velocity) / (np.linalg.norm(env.target_lin_velocity) ** 2)) * env.target_lin_velocity)
-    total_normalized_effort = np.sum([(j.effort / j.max_torque) ** 2 for j in env.agent.motor_joints]) / len(env.agent.motor_joints)
 
     # Derived penalties
     lin_vel_reward = max(1 - 1.75 * lin_vel_error, -1.0)
@@ -36,12 +33,11 @@ def reward_function(env: SpotmicroEnv, action: np.ndarray) -> tuple[float, dict]
 
     # === Final Reward ===
     reward_dict = {
-        "linear_vel_reward": 10 * lin_vel_reward,
+        "linear_vel_reward": 11 * lin_vel_reward,
         "height_penalty": -3 * min(height_penalty, 1.0),
         "stabilization_penalty": -3 * min(stabilization_penalty, 1.0),
-        "drift_penalty": -3 * fade_in(env.num_steps, 7_000_000, scale=2.5) * drift_penalty,
+        "drift_penalty": -2 * drift_penalty,
         "angular_vel_penalty": -1.5 * ang_vel_error,
-        "effort_penalty": -1.5 * fade_in(env.num_steps, 7_000_000, scale=2.5) * total_normalized_effort,
         "deviation_penalty": -0.5 * deviation_penalty,
     }
     total_reward = sum(reward_dict.values())
