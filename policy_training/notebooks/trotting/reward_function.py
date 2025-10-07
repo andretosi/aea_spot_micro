019a88ca568e5563ed4341c642d47107f4533907
Nicolo Dellepiane <nicolo.dellepiane@mail.polimi.it>
2025-10-07 23:50:59 +0200

    increased action rate

--- policy_training/notebooks/trotting/reward_function.py
+++ policy_training/notebooks/trotting/reward_function.py
@@ -48,9 +48,9 @@ def reward_function(env: SpotmicroEnv, action: np.ndarray) -> tuple[float, dict]
         "linear_vel_reward": 12.5 * lin_vel_reward,
         "height_penalty": -3 * min(height_penalty, 1.0),
         "stabilization_penalty": -3 * min(stabilization_penalty, 1.0),
+        "action_rate_penalty": -2 * action_rate,
         "drift_penalty": -2 * drift_penalty,
         "angular_vel_penalty": -1.5 * ang_vel_error,
-        "action_rate_penalty": -1 * action_rate,
         "effort_penalty": -1 * normalized_effort,
         "deviation_penalty": -0.5 * deviation_penalty,
     }
