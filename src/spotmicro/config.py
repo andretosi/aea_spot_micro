67eed9a483e5b8add747b67dc3bdf87e746fd60d
Nicolo Dellepiane <nicolo.dellepiane@mail.polimi.it>
2025-11-26 22:51:40 +0100

    Attributes are now stored as private, and a property (getter, setter) is created for each one of them. Added support for function assignment to parameters

--- src/spotmicro/config.py
+++ src/spotmicro/config.py
@@ -1,5 +1,15 @@
 import yaml
 
+class ValueProvider:
+    def __init__(self, value_or_func):
+        if callable(value_or_func):
+            self.func = value_or_func
+        else:
+            self.func = lambda: value_or_func
+    
+    def get(self):
+        return self.func()
+
 class Config:
     """
     questa classe serve per assegnare i valori presenti nel file di configurazione yaml
@@ -19,4 +29,11 @@ class Config:
             cfg_dict = yaml.safe_load(f)
 
         for key, value in cfg_dict.items():
-            setattr(self, key, value)
+            setattr(self, key, ValueProvider(value))
+            
+            def make_setter(name):
+                def setter(self, value: ValueProvider):
+                    setattr(self, name, value)
+                return setter
+            
+            setattr(self, f"set_{key}", make_setter(key)) #Closes over name! Assign a setter for each attribute
