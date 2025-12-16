0c5eb86b5ba9a46832e3d848044eb1279e39e63f
Nicolo Dellepiane <nicolo.dellepiane@mail.polimi.it>
2025-12-16 23:13:22 +0100

    layed out "configurable" decorator

--- src/spotmicro/tools/configurable.py
+++ src/spotmicro/tools/configurable.py
@@ -2,8 +2,6 @@ from spotmicro.tools.config import Config
 import inspect
 from functools import wraps
 
-CONFIGURABLE_REGISTRY = []
-
 def configurable(cls):
     """
     Decorator function that extends the behaviour of the decorated class by wrapping its init and adding a config attribute.
@@ -12,23 +10,30 @@ def configurable(cls):
     :param cls: This class is any object that can be configured, and as such should be linked to the central registry
     """
     
-    CONFIGURABLE_REGISTRY.append(cls) #Register the class, to eventually generate the parametric dataclass
+    original_init = cls.__init__
+    init_sig = inspect.signature(original_init) #extract consstructor signature so that we can copypaste it on our wrapper
 
-    original_init = cls.__init__ #store the original init somewhere before modifying it with config logic
-    sig = inspect.signature(original_init) #extract its signature so that we can copypaste it on our wrapper
+    params = {name: param.default for name, param in init_sig.parameters.items() if name != "self"}
+    if "config" not in params.keys():
+        raise ValueError(f"Every configurable class must have a positional argument named config. ({cls})") 
+    elif not isinstance(params["config"], Config):
+        raise TypeError(f"config parameter must be of Config type, was given type: {type(params['config'])}")
+
     
     #extract kwargs from the constructor. Used to build overridden_params dict
-    default_params = {name: param.default for name, param in sig.parameters.items() 
-                if name != "self" and param.default is not inspect.Parameter.empty}
+    default_params = {name: param.default for name, param in params if param.default is not inspect.Parameter.empty}
+    if "config" in default_params.keys():
+        raise ValueError(f"Parameter config must be positional argument, was given as keywprd argument inside ({cls})")
+    
 
     #<----- WRAPPER ----->
-    @wraps(original_init)#used to preserve everything about the wrapped class and pass it to the wrapper. Everything, but the signature
-    def __init__(self, *args, config : Config = None, **kwargs):
+    @wraps(original_init)#used to preserve everything about the wrapped class and pass it to the wrapper
+    def __init__(self, config: Config, *args, **kwargs):
         """
         This is a wrapper constructor that basically injects some config logic around the constructor of the given class
         """
         #These lines do not modify the class!!
-        bound = sig.bind(self, *args, **kwargs) #Map positional and keyword arguments to the corect parameter. Returns a dict
+        bound = init_sig.bind(self, *args, **kwargs) #Map positional and keyword arguments to the corect parameter. Returns a dict
         bound.apply_defaults()#Fill the above map with default values when none were provided
         
         #extract params and their runtime value
@@ -39,14 +44,37 @@ def configurable(cls):
         
         original_init(self, *args, **kwargs) #run the original constructor
 
-        if config is None:
-            config = Config() #Generate an empty config to fill with default parameters and overrides 
-        
         config.register(cls, self, overridden_params) #register the initialized instance to config
         self.config = config
     #<----- END WRAPPER ----->
 
-    #change the class constructor with our wrapper
+    #<----- SAVE LOGIC ------>
+    #TODO
+    def save(self, path : str):
+        """
+        Save the parameters of **all** configurable objects to the given file
+
+        :param path: path to the file to dump the config in.
+        :type path: str
+        """
+        pass
+    #<----- END SAVE   ------>
+    #<----- LOAD LOGIC ------>
+    #TODO
+    def load(self, path: str):
+        """
+        Override the current config for this component with the parameters defined in another config file
+
+        :param path: Description
+        :type path: str
+        """
+        pass
+    #<----- END LOAD   ------>
+
+
+    #change the class constructor with our wrapper and add functions
     cls.__init__ = __init__
+    cls.load = load
+    cls.save = save
 
     return cls
