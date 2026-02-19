ebe0f5d0ee8b210fef09130ddd3b64fc965cf35a
Nicolo Dellepiane <nicolo.dellepiane@mail.polimi.it>
2026-02-19 13:04:01 +0100

    added second integration test to check that parameters are set and overridden correctly by constructors and configs

--- src/spotmicro/tools/config.py
+++ src/spotmicro/tools/config.py
@@ -43,8 +43,6 @@ class Config:
         
 
     def save(self, dst_filepath: str) -> None:
-        if not os.path.exists(dst_filepath):
-            print("oopds")
         with open(dst_filepath, "w") as f:
             yaml.safe_dump(self.central_registry, f, default_flow_style=False)
 
@@ -53,7 +51,6 @@ class Config:
         """
         Add the parameters of an instance of a configurable class to the registry, in its namespace.\n
         Returns a dictionary holding all parameters relating to the given class *set in the config file*, excluding those overridden by the constructor.
-
         
         :param component_type: class of the component
         :param component_instance: specific instance of the component
