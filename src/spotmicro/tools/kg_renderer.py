88ae1f3fa4f6a2ff08dbf9a5588c8616eead2742
Filippo <filippo.roncone@mail.polimi.it>
2026-03-09 19:06:55 +0100

    new debug arrow

--- src/spotmicro/tools/kg_renderer.py
+++ src/spotmicro/tools/kg_renderer.py
@@ -21,15 +21,18 @@ class KG_Renderer(ABC):
         pass
 
 
-
 class PyBulletRenderer(KG_Renderer):
 
     def __init__(self, client_id):
-
         self.client_id = client_id
         self.line_id = None
+        self.shaft_id = None
+        self.head_id = None
+        self.cone1_id = None
+        self.cone2_id = None
 
     def spawn(self):
+        
         self.line_id = p.addUserDebugLine(
             lineFromXYZ=[0,0,0],
             lineToXYZ=[0,0,0.001],
@@ -37,19 +40,119 @@ class PyBulletRenderer(KG_Renderer):
             lineWidth=3,
             physicsClientId=self.client_id
         )
-    
+
     def update(self, position, orientation, dt:float):
-        """
-        Aggiorna posizione e orientamento del ghost
-        """
-        # calcola direzione della freccia dal quaternion
+        # Corpo della freccia
+        if self.shaft_id is None:
+            shaft_vs = p.createVisualShape(
+                p.GEOM_CYLINDER,
+                radius=0.005,
+                length=0.15,
+                rgbaColor=[1, 0, 0, 1],
+                physicsClientId=self.client_id
+            )
+            self.shaft_id = p.createMultiBody(
+                baseMass=0,
+                baseVisualShapeIndex=shaft_vs,
+                basePosition=[0.075, 0, 0.5],
+                baseOrientation=p.getQuaternionFromEuler([0, np.pi/2, 0]),
+                physicsClientId=self.client_id
+            )
+
+        # Anello base cono
+        if self.head_id is None:
+            head_vs = p.createVisualShape(
+                p.GEOM_CYLINDER,
+                radius=0.01,
+                length=0.01,
+                rgbaColor=[1, 0, 0, 1],
+                physicsClientId=self.client_id
+            )
+            self.head_id = p.createMultiBody(
+                baseMass=0,
+                baseVisualShapeIndex=head_vs,
+                basePosition=[0.15, 0, 0.5],
+                baseOrientation=p.getQuaternionFromEuler([0, np.pi/2, 0]),
+                physicsClientId=self.client_id
+            )
+
+        # Anello medio cono
+        if self.cone1_id is None:
+            cone1_vs = p.createVisualShape(
+                p.GEOM_CYLINDER,
+                radius=0.005,
+                length=0.01,
+                rgbaColor=[1, 0, 0, 1],
+                physicsClientId=self.client_id
+            )
+            self.cone1_id = p.createMultiBody(
+                baseMass=0,
+                baseVisualShapeIndex=cone1_vs,
+                basePosition=[0, 0, 0.5],
+                baseOrientation=p.getQuaternionFromEuler([0, np.pi/2, 0]),
+                physicsClientId=self.client_id
+            )
+
+        # Punta finale cono
+        if self.cone2_id is None:
+            cone2_vs = p.createVisualShape(
+                p.GEOM_CYLINDER,
+                radius=0.0025,
+                length=0.005,
+                rgbaColor=[1, 0, 0, 1],
+                physicsClientId=self.client_id
+            )
+            self.cone2_id = p.createMultiBody(
+                baseMass=0,
+                baseVisualShapeIndex=cone2_vs,
+                basePosition=[0, 0, 0.5],
+                baseOrientation=p.getQuaternionFromEuler([0, np.pi/2, 0]),
+                physicsClientId=self.client_id
+            )
+
         rot_matrix = p.getMatrixFromQuaternion(orientation)
-        direction = np.array([rot_matrix[0], rot_matrix[1], rot_matrix[2]])  # asse x
+        direction = np.array([rot_matrix[0], rot_matrix[1], rot_matrix[2]])
 
         draw_pos = position.copy()
-        draw_pos[2] = 0.5  # altezza fissa sopra il terreno
+        draw_pos[2] = 0.5
 
-        end = draw_pos + 0.3 * direction
+        rot_to_x = p.getQuaternionFromEuler([0, np.pi/2, 0])
+        _, shaft_orientation = p.multiplyTransforms(
+            [0, 0, 0], orientation,
+            [0, 0, 0], rot_to_x
+        )
+
+        # Cilindro: centro a metà lunghezza
+        shaft_pos = np.array(draw_pos) - direction * 0.075
+        p.resetBasePositionAndOrientation(
+            self.shaft_id, shaft_pos.tolist(), shaft_orientation,
+            physicsClientId=self.client_id
+        )
+
+        # Anello base: fine cilindro + metà sua lunghezza
+        head_pos = np.array(draw_pos) - direction * 0.155
+        p.resetBasePositionAndOrientation(
+            self.head_id, head_pos.tolist(), shaft_orientation,
+            physicsClientId=self.client_id
+        )
+
+        # Anello medio: fine head + metà sua lunghezza
+        cone1_pos = np.array(draw_pos) - direction * 0.165
+        p.resetBasePositionAndOrientation(
+            self.cone1_id, cone1_pos.tolist(), shaft_orientation,
+            physicsClientId=self.client_id
+        )
+
+        # Punta: fine cone1 + metà sua lunghezza
+        cone2_pos = np.array(draw_pos) - direction * 0.1725
+        p.resetBasePositionAndOrientation(
+            self.cone2_id, cone2_pos.tolist(), shaft_orientation,
+            physicsClientId=self.client_id
+        )
+
+        '''
+        # Linea originale
+        end = draw_pos - 0.3 * direction
 
         if self.line_id is not None:
             p.removeUserDebugItem(self.line_id, physicsClientId=self.client_id)
@@ -59,13 +162,10 @@ class PyBulletRenderer(KG_Renderer):
             end,
             [1, 0, 0],
             3,
-            #replaceItemUniqueId=self.line_id,
             lifeTime=0.3,
             physicsClientId=self.client_id
-        ) 
+        )
+        '''
 
     def reset(self, start_pos, start_quat):
-        """
-        Reset del ghost
-        """
         self.update(start_pos, start_quat, dt=0.1)
