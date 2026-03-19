da3315adb93edc064ae36b7b44582de0b9ca452b
Mirko Berardi <mirko.berardi@mail.polimi.it>
2026-03-19 16:38:30 +0100

    new uppdated joystick based on pygame

--- src/spotmicro/devices/joystick_device.py
+++ src/spotmicro/devices/joystick_device.py
@@ -2,76 +2,62 @@
 
 from spotmicro.devices.device import Device
 from spotmicro.agent.input import Input
+import pygame
+import numpy as np
 
-
+'''This device reads inputs from a common gamepad.
+this program makes use of the Pygame libray to read 
+the position of the joysticks and normalize them to
+communicate the velocities and transmit them in the format:
+    left joystick [vertical]    -> vel_x {speed in the march direction}
+    left joystick [horizontal]  -> vel_y {speed in the perpendicoular direction}
+    left joystick [horizontal]  -> vel_w {angoular velocity with regard to the yaw angle}
+'''
 class Joystick(Device):
-    def __init__(self):
-        import Gamepad
-        import time
+    def __init__(self, joystick_ID: int = 0):
+        pygame.init()
+        pygame.joystick.init()
 
-        # Gamepad settings
-        gamepadType = Gamepad.PS3
-        buttonExit = 'PS'
-        vertical = 'LEFT-Y'
-        horizzontal = 'LEFT-X'
-        angoular = 'RIGHT-X'
-        pollInterval = 0.2
+        if pygame.joystick.get_count() ==0:
+            raise RuntimeError ("No joystick found! Please connect a controller")
 
-        # Wait for a connection
-        if not Gamepad.available():
-            print('Please connect your gamepad...')
-            while not Gamepad.available():
-                time.sleep(1.0)
-        gamepad = gamepadType()
-        print('Gamepad connected')
+        self.joy = pygame.joystick.Joystick(joystick_ID)
+        self.joy.init()
 
-        # Set some initial state
-        global running
-        running = True
-        self.x = 0.0
-        self.y = 0.0
-        self.w = 0.0
+        #Initialize the Input array
+        self._input_vector = np.array([0.00, 0.00, 0.00], dtype = np.float32)
 
-        # Create some callback functions
-        def exitButtonPressed():
-            global running
-            print('EXIT')
-            running = False
+    def apply_deadzone (self, val, threshold = 0.1):
+        if abs(val) < threshold:
+            return 0.00
+        return(val - np.sign(val) * threshold) / (1.0 - threshold)
 
-        def verticalAxisMoved(position):
-            global sideway
-            self.x = -position #for some reason the guide suggest to invert linear velocities but not angoular
+    def update(self) -> None:
+        pygame.event.pump()
 
-        def horizontalAxisMoved(position):
-            global foward
-            self.y = -position
-        
-        def angoularAxisMoved(position):
-            global steering
-            self.w = position
+        # read the axis position 
 
+        # Test rapido assi (cortesia di gemini)
+        #for i in range(self.joy.get_numaxes()):
+        #    print(f"Asse {i}: {self.joy.get_axis(i):.2f}", end=" | ")
+        #print()
 
-        # Start the background updating
-        gamepad.startBackgroundUpdates()
+        # in Pygame vertical axis is inverted(-1 is up and +1 is down), so there has been added a minus (-)
+    
+        vel_x = -self.joy.get_axis(1) # vertical left stick for up and down
+        vel_y = -self.joy.get_axis(0) # horizontal left stick for right and left
+        vel_w = -self.joy.get_axis(3) # horizontal right stick for rotation {might also be axis 4}
 
-        # Register the callback functions
-        gamepad.addButtonPressedHandler(buttonExit, exitButtonPressed)
-        gamepad.addAxisMovedHandler(vertical, verticalAxisMoved)
-        gamepad.addAxisMovedHandler(horizontal, horizontalAxisMoved)
-        gamepad.addAxisMovedHandler(angoular, angoularAxisMoved)
-
-        # Keep running while joystick updates are handled by the callbacks
-        try:
-            while running and gamepad.isConnected():
-                # Show the current speed and steering
-                print('%+.1f %% self.x, %+.1f %% self.y, %+.1f %% self.w' % (x * 100, y * 100, w*100))
-
-
-                # Sleep for our polling interval
-                time.sleep(pollInterval)
-            finally:
-                # Ensure the background thread is always terminated when we are done
-                gamepad.disconnect()
+        # as a safe precaution, ther has been added a dead zone
+        # it is needed to prevent that the robot moves as result
+        # of external disturbances on the sticks
+        self._input_vector[0] = self.apply_deadzone (vel_x)
+        self._input_vector[1] = self.apply_deadzone (vel_y)
+        self._input_vector[2] = self.apply_deadzone (vel_w)
         
 
-        raise NotImplementedError("Joystick device is not implemented yet")
+    def read(self) -> Input:
+        return self._input_vector
+
+    def reset(self) -> None:
+        self._input_vector = np.array([0.00, 0.00, 0.00], dtype=np.float32)
